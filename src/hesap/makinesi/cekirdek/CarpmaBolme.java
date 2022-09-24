861044e3df908e96c060061578bd5424f1e76d15
Smtgny25 <sametguney6396@gmail.com>
2022-09-24 17:12:31 +0300

    carpma ve bolme metodlari hazirlandi

--- src/hesap/makinesi/cekirdek/CarpmaBolme.java
+++ src/hesap/makinesi/cekirdek/CarpmaBolme.java
@@ -9,5 +9,17 @@ package hesap.makinesi.cekirdek;
  * @author kadir
  */
 public class CarpmaBolme {
+    // çarpma bölme işlemi smtgny tarafından yapılacak.
+    public double carpma (double a, double b) {
+        double carpim = a*b;
+        return carpim;
+    }
+    public double bolme (double a, double b) {
+        double bolum = doubel (a/b);
+        return bolum;
+    }
+    
+        
+    
     
 }
