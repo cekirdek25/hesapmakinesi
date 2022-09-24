6a6be7da8520fd5881daea910afa1e374cbad16a
aktibaba <abd.akti@gmail.com>
2022-09-24 08:31:07 -0500

    toplama cikarma ve carpmabolme classlari eklendi

--- src/hesap/makinesi/cekirdek/HesapMakinesiCekirdek.java
+++ src/hesap/makinesi/cekirdek/HesapMakinesiCekirdek.java
@@ -12,7 +12,7 @@ public class HesapMakinesiCekirdek {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+      
     }
     
 }
