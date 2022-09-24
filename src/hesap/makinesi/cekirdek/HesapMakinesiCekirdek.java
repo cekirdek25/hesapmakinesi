a73e0f24265ec11db8de43f6bb7c954c5b8dfee2
aktibaba <abd.akti@gmail.com>
2022-09-24 08:18:29 -0500

    first commit hesap makinesi create edildi

---
+++ src/hesap/makinesi/cekirdek/HesapMakinesiCekirdek.java
@@ -0,0 +1,18 @@
+
+
+package hesap.makinesi.cekirdek;
+
+/**
+ *
+ * @author kadir
+ */
+public class HesapMakinesiCekirdek {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
