64c25afeccb2d769c5c443ef6490a9e731f7bd30
unver25 <muhammetunver25@gmail.com>
2022-10-01 17:16:23 +0300

    baskanim karismis

--- src/hesap/makinesi/cekirdek/CarpmaBolme.java
+++ src/hesap/makinesi/cekirdek/CarpmaBolme.java
@@ -15,21 +15,6 @@ public class CarpmaBolme {
 
 
 
-public int toplama (int a,int b)  {
-    
-    
-int toplam = a + b;
-return toplam;
-    
-}
 
-
-public int cikarma (int a,int b){
-
-int cikarma = a-b;
-return cikarma;
-
-
-}
 
 }
--- src/hesap/makinesi/cekirdek/ToplamaCikarma.java
+++ src/hesap/makinesi/cekirdek/ToplamaCikarma.java
@@ -3,8 +3,22 @@ package hesap.makinesi.cekirdek;
 
 /**
  *
- * @author kadir
+ * @author mami
  */
 public class ToplamaCikarma {
+public int toplama (int a,int b)  {
     
+int toplam = a + b;
+return toplam;
+    
+}
+
+
+public int cikarma (int a,int b){
+
+int cikarma = a-b;
+return cikarma;
+
+
+}    
 }
